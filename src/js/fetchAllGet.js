d08eee5ca7fe7600320ffced91dd5505700fa13c
Dariia96 <darynastakhnyak@gmail.com>
2023-06-10 22:05:29 +0300

    add js and html to hero

--- src/js/fetchAllGet.js
+++ src/js/fetchAllGet.js
@@ -1,1 +1,13 @@
 import axios from 'axios';
+
+async function serviceFilm() {
+    const BASE_URL = "https://api.themoviedb.org/3";
+    const ENDPOINT = "/trending/movie/day";
+    const API_KEY = "5bf13f442a6612ea903461e28536fdca";
+
+    const hero_film = await axios.get(`${BASE_URL}${ENDPOINT}?api_key=${API_KEY}`);
+
+    return hero_film;
+    ;
+}
+export { serviceFilm };
--- src/js/refs.js
+++ src/js/refs.js
@@ -3,4 +3,9 @@ export const refs = {
   menuContainer: document.querySelector('data-menu'),
   menuBackdrop: document.querySelector('[data-backdrop]'),
   body: document.querySelector('body'),
+/*----------------------------HERO--------------------------*/
+home_hero_film_wraper: document.querySelector('.hero-content'),
+hero_trailer_btn: document.querySelector('.hero-trailer-btn'),
+go_to_catalog_btn: document.querySelector('.go-to-catalog-btn'),
+home_hero: document.querySelector('.home-hero'),
 };
