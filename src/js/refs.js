ef45fff1f56cf0d0fe9849a84797328747739db4
IhorHodebskyi <121040851+IhorHodebskyi@users.noreply.github.com>
2023-06-10 21:27:48 +0300

    resf
    refs

--- src/js/refs.js
+++ src/js/refs.js
@@ -1,4 +1,4 @@
-export default refs = {
+export const refs = {
   menuBtn: document.querySelector('[data-menu-open]'),
   menuContainer: document.querySelector('data-menu'),
   menuBackdrop: document.querySelector('[data-backdrop]'),
