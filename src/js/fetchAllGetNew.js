028eac61eaae34847524a39794244cbc00fa1aee
Serhii Ozeran <ozeransm@gmail.com>
2023-06-11 18:33:58 +0300

    update css btn, del fetchAllGetNew

--- src/js/fetchAllGetNew.js
+++
@@ -1,7 +0,0 @@
-import axios from 'axios';
-
-async function fetchAllGet(BASE_URL, ENDPOINT='', API_KEY='', QUERY='') {
-        const hero_film = await axios.get(`${BASE_URL}${ENDPOINT}?api_key=${API_KEY}&${QUERY}`);
-        return hero_film;
-    }
-export { fetchAllGet };
