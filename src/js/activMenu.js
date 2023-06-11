349e5945259d0e48269a8ed19a44ba19a68b33e9
IhorHodebskyi <121040851+IhorHodebskyi@users.noreply.github.com>
2023-06-11 18:47:44 +0300

    add

--- src/js/activMenu.js
+++ src/js/activMenu.js
@@ -5,7 +5,7 @@ if (currentPath === '/project-neverland/index.html') {
   navHeader.children[0].children[0].classList.add('nav_list-current');
 } else if (currentPath === '/project-neverland/catalog.html') {
   navHeader.children[1].children[0].classList.add('nav_list-current');
-} else if (currentPath === 'project-neverland/library.html') {
+} else if (currentPath === '/project-neverland/library.html') {
   navHeader.children[2].children[0].classList.add('nav_list-current');
 } else {
   navHeader.children[0].children[0].classList.add('nav_list-current');
