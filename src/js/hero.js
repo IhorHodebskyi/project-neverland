ffc20106815fef6b54dc903e3608db2d3a15ea11
IhorHodebskyi <121040851+IhorHodebskyi@users.noreply.github.com>
2023-06-11 15:53:44 +0300

    add

--- src/js/hero.js
+++ src/js/hero.js
@@ -1,35 +1,36 @@
-import axios from "axios";
-import { serviceFilm } from "./fetchAllGet";
-import { refs } from "./refs";
-/*async function serviceFilm() {
-    const BASE_URL = "https://api.themoviedb.org/3";
-    const ENDPOINT = "/trending/movie/day";
-    const API_KEY = "5bf13f442a6612ea903461e28536fdca";
+import axios from 'axios';
+import { serviceFilm } from './fetchAllGet';
+import { refs } from './refs';
+// /*async function serviceFilm() {
+//     const BASE_URL = "https://api.themoviedb.org/3";
+//     const ENDPOINT = "/trending/movie/day";
+//     const API_KEY = "5bf13f442a6612ea903461e28536fdca";
 
-    const hero_film = await axios.get(`${BASE_URL}${ENDPOINT}?api_key=${API_KEY}`);
+//     const hero_film = await axios.get(`${BASE_URL}${ENDPOINT}?api_key=${API_KEY}`);
 
-    return hero_film;
-    ;
-}
-export { serviceFilm };*/
-
-
+//     return hero_film;
+//     ;
+// }
+// export { serviceFilm };*/
 
 serviceFilm()
-    .then(data => {
-        const firstFilm = (data.data.results)[0];
-        console.log(firstFilm);
-        if (!firstFilm) {
-            refs.homeHero.classList.remove('is-hidden');
-        }
-        refs.homeHeroFilmWraper.insertAdjacentHTML('beforeend', createMarkup(firstFilm));
+  .then(data => {
+    const firstFilm = data.data.results[0];
+    console.log(firstFilm);
+    if (!firstFilm) {
+      refs.homeHero.classList.remove('is-hidden');
+    }
+    refs.homeHeroFilmWraper.insertAdjacentHTML(
+      'beforeend',
+      createMarkup(firstFilm)
+    );
 
-        refs.homeHero.classList.add('is-hidden');
+    refs.homeHero.classList.add('is-hidden');
 
-        refs.heroTrailerBtn.setAttribute("id", `${firstFilm.id}`);
-        refs.goToCatalogBtn.setAttribute("id", `${firstFilm.id}`);
-    })
-    .catch(err => console.log(err))
+    refs.heroTrailerBtn.setAttribute('id', `${firstFilm.id}`);
+    refs.goToCatalogBtn.setAttribute('id', `${firstFilm.id}`);
+  })
+  .catch(err => console.log(err));
 
 /*function createMarkup(arr) {
     return arr.map(({ original_title, poster_path, vote_average, overview }) => 
@@ -42,12 +43,11 @@ serviceFilm()
     
 }*/
 
-function createMarkup({ original_title, poster_path, vote_average, overview}) {
-    return `<div class = "home-hero-film" >
+function createMarkup({ original_title, poster_path, vote_average, overview }) {
+  return `<div class = "home-hero-film" >
     <img src="https://image.tmdb.org/t/p/w500${poster_path}" alt="${original_title}" class = "home-hero-poster">
     <h2 class = "home-hero-title">${original_title}</h2>
     <p class = "home-hero-overview">${overview}</p>
     <p class = "home-hero-rate">${vote_average}</p>
-    </div>`
+    </div>`;
 }
-
