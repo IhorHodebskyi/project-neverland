644081162b59b81b6154e7a2a778e9f325868a27
Dima Levchenko <112283201+levandowskiy@users.noreply.github.com>
2023-06-14 02:22:29 +0300

    fix theme switcher

--- src/js/filter-years.js
+++ src/js/filter-years.js
@@ -1,9 +1,12 @@
 const selectBtnEl = document.querySelector(".select-btn");
 const optionsEl = document.querySelector(".options")
 const bodyEl = document.querySelector(".body");
+const arrowEl = document.querySelector(".icon-arrow-down")
 
 selectBtnEl.addEventListener("click", () => {
     optionsEl.classList.toggle("hidden")
+    arrowEl.classList.toggle("animation")
+
     createMarkupOption()
 })
 
@@ -11,9 +14,15 @@ function createMarkupOption() {
     const currentYear = new Date().getFullYear();
     const oldestFilmYear = 1874;
     let optionsMarkup = '';
+    let whiteThemeClass = '';
+
+    const isWhiteTheme = localStorage.getItem("theme") === "light";
+    if(isWhiteTheme) {
+        whiteThemeClass = "light-theme__section--secondaryText";
+    } 
 
     for (let year = currentYear; year >= oldestFilmYear; year--) {
-        optionsMarkup += `<li class="option">${year}</li>`;
+        optionsMarkup += `<li class="option ${whiteThemeClass}">${year}</li>`;
     }
 
     optionsEl.innerHTML = optionsMarkup;
