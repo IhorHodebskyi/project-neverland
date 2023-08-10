705f1a7db660137a7d0dcbc21caddf702779bfb1
EvgenBatt <evgenmgid@gmail.com>
2023-08-11 00:04:06 +0300

    Delete console.log`s in file

--- src/js/weekly.js
+++ src/js/weekly.js
@@ -1,6 +1,5 @@
 import axios from 'axios';
 import { refs } from './refs';
-import ratingStarFull from '../images/reitingfull.svg';
 import { genreStr } from './month';
 import { handlerClickcardsSectionBackphoto } from './modal-film';
 import starIconFull from '../images/reitingfull.svg';
@@ -17,31 +16,22 @@ async function fetchTrendsOfWeek() {
   const response = await axios.get(
     `${BASE_URL}?api_key=${API_KEY}&language=en-US&page=${randomIndex}`
   );
-  console.log(randomIndex);
   originalData = response.data.results;
   handleResponsive(originalData);
-  console.log(originalData);
   return originalData;
 }
-// window.addEventListener('resize', handleResponsive);
+
 function handleResponsive() {
   const screenWidth = window.innerWidth;
   let slicedData = [];
-  console.log(slicedData);
-  // if (screenWidth < 768) {
-  //   return (slicedData = originalData.slice(0, 1));
-  // }
-  // slicedData = originalData.slice(0, 3);
+
   slicedData = originalData.slice(0, 3);
   createMarkup(slicedData);
 }
 
-// fetchTrendsOfWeek();
-
 export { fetchTrendsOfWeek };
 
 async function createMarkup(data) {
-  console.log(data);
   data.map(
     async (
       {
@@ -106,97 +96,4 @@ async function createMarkup(data) {
       }
     }
   );
-
-  //  list.insertAdjacentHTML('beforeend', cardMarkUp);
-
-  //   refs.cardsSectionBackphoto = document.querySelector('.card-film');
-  //   console.log(refs.cardsSectionBackphoto);
-  //   refs.cardsSectionBackphoto.forEach(el =>
-  //   el.addEventListener('click', handlerClickcardsSectionBackphoto)
-  // );
 }
-
-// function createMarkupCard(cardresult) {
-//   const cardMarkUp = cardresult.map(
-//     async (
-//       {
-//         original_title,
-//         poster_path,
-//         vote_average,
-//         genre_ids,
-//         release_date,
-//         id,
-//       },
-//       i
-//     ) => {
-//       const genresLine = await genreStr(genre_ids);
-//       const str = `<a href="#" class="card-film" id="${id}">
-//         <div class="card-backdrop"></div>
-//         <img
-//           class="card-img"
-//           src="https://image.tmdb.org/t/p/w500${poster_path}"
-//           alt=""
-//           loading="lazy"
-//           srcset="
-//             https://image.tmdb.org/t/p/w500${poster_path} 1x,
-//             https://image.tmdb.org/t/p/w500${poster_path} 2x
-//           "
-//         />
-//         <div class="card-info-section">
-//           <h3 class="card-info-title">${original_title}</h3>
-//           <div class="card-info">
-//             <p class="card-info-text">
-//             ${genresLine} | ${String(release_date).slice(0, 4)}
-//             </p>
-//             <ul class="card-vote">
-//               <li class="card-vote-items">
-//                 <img
-//                   class="card-vote-icon"
-//                   src="${ratingStarFull}"
-//                   alt="Rating Stars"
-//                 />
-//               </li>
-//               <li class="card-vote-items">
-//                 <img
-//                   class="card-vote-icon"
-//                   src="${ratingStarFull}"
-//                   alt="Rating Stars"
-//                 />
-//               </li>
-//               <li class="card-vote-items">
-//                 <img
-//                   class="card-vote-icon"
-//                   src="${ratingStarFull}"
-//                   alt="Rating Stars"
-//                 />
-//               </li>
-//               <li class="card-vote-items">
-//                 <img
-//                   class="card-vote-icon"
-//                   src="${ratingStarFull}"
-//                   alt="Rating Stars"
-//                 />
-//               </li>
-//               <li class="card-vote-items">
-//                 <img
-//                   class="card-vote-icon"
-//                   src="${ratingStarFull}"
-//                   alt="Rating Stars"
-//                 />
-//               </li>
-//             </ul>
-//           </div>
-//         </div>
-//       </a>`;
-//       refs.weeklyList.insertAdjacentHTML('beforeend', str);
-//       if (2 === i) {
-//         refs.cardsSectionBackphoto = document.querySelectorAll('.card-film');
-//         refs.cardsSectionBackphoto.forEach(el =>
-//           el.addEventListener('click', handlerClickcardsSectionBackphoto)
-//         );
-//       }
-//     }
-//   );
-
-//   // refs.weeklyList.insertAdjacentHTML('beforeend', cardMarkUp);
-// }
